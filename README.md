2571a01786e33d1a0da28d0981e6c2283990d08d
Peter <pndungu54@gmail.com>
2018-06-21 21:06:18 +0300

    -Remove older version of app

--- README.md
+++
@@ -1,2 +0,0 @@
-## Purpose
-    ride-my-way App is a carpooling application that provides drivers with the ability to create ride offers and passengers to join available ride offers
--- UI/static/js/driver_dashboard.js
+++
@@ -1,7 +0,0 @@
-function openNav() {
-    document.getElementById("dash-sidenav").style.width = "250px";
-}
-
-function closeNav() {
-    document.getElementById("dash-sidenav").style.width = "0";
-}
