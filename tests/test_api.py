1fcade746ca186cd0daa8f1907120a2d759d6dde
Peter <pndungu54@gmail.com>
2018-06-22 23:48:29 +0300

    tests(api endpoints)
    -Add minimum endpoints for api

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,61 +1,68 @@
 """Contains tests for api endpoints."""
-from app.api import views
 
 import unittest
 
+from app import create_app
 
-class TestApi (unittest.TestCase):
+
+class TestApi(unittest.TestCase):
     """Tests the api endpoints."""
 
     def setUp(self):
         """Initialize objects for the tests."""
-        self.ride = {
-                     "Destination": "Meru",
+        self.ride = {"Destination": "Meru",
                      "Origin": "Kutus",
                      "Time": "9:00",
-                     "Date": "23-6-2018"
-                     }
+                     "Date": "23-6-2018"}
+        self.request = {
+            "Passenger Name": "Njobu",
+            "Tel": "+254716272376"
+        }
+        self.app = create_app('testing')
+        self.client = self.app.test_client
+        self.app.context.push()
 
     def tearDown(self):
         """Remove objects after test."""
+        self.app.context.pop()
         del self.ride
-
-    def test_get_all_rides(self):
-        """Test get all rides successfuly."""
-        pass
-
-    def test_make_ride_request(self):
-        """Test user can make a request successfuly."""
-        pass
-
-    def test_get_ride(self):
-        """Test user can get a ride successfuly."""
-        pass
 
     def test_create_ride(self):
         """Test user can create a ride successfuly."""
-        pass
+        response = self.client().post('/api/v1/rides', data=self.ride)
+        self.assertEqual(response, 201)
+        self.assertIn('Meru', str(response.data))
 
-    def test_json_request_only(self):
-        """Test the request data is in json format."""
-        pass
+    def test_get_ride(self):
+        """Test user can get a ride successfuly."""
+        response = self.client().post('/api/v1/rides', data=self.ride)
+        self.assertEqual(response, 201)
+        res = self.client().get('api/v1/rides/1')
+        self.assertEqual(res, 200)
+        self.assertIn('Meru', str(res.data))
 
-    def test_required_data_in_request(self):
-        """Test request data contains all the necessary fields."""
-        pass
+    def test_get_all_rides(self):
+        """Test get all rides offers successfuly."""
+        response = self.client().post('/api/v1/rides', data=self.ride)
+        self.assertEqual(response, 201)
+        res = self.client().get('api/v1/rides')
+        self.assertEqual(res, 200)
+        self.assertIn('Kutus', str(res.data))
 
-    def test_date_data(self):
-        """Test date is formatted correctly."""
-        pass
+    def test_make_ride_request(self):
+        """Test user can make a request successfuly."""
+        response = self.client().post('/api/v1/rides', data=self.ride)
+        self.assertEqual(response, 201)
+        res = self.client().post('api/v1/rides/1/requests', data=self.request)
+        self.assertEqual(res, 201)
+        self.assertIn('Njobu', str(res.data))
 
-    def test_location_in_string(self):
-        """Test location data in string format."""
-        pass
-
-    def test_request_inexistent_ride(self):
-        """Test user is notified if requested ride does not exist."""
-        pass
-
-    def test_user_notified_of_ride_request(self):
-        """Test a user is notified when the driver accepts or rejects request."""
-        pass
+    def test_get_ride_requests(self):
+        """Test driver can view a ride's request."""
+        response = self.client().post('/api/v1/rides', data=self.ride)
+        self.assertEqual(response, 201)
+        res = self.client().post('api/v1/rides/1/requests', data=self.request)
+        self.assertEqual(res, 201)
+        result = self.client().get('api/v1/rides/1/requests')
+        self.assertEqual(result, 200)
+        self.assertIn(result, '+254716272376')
