77d872e783bf31722da1f0cbc2893d196676cda8
Peter <pndungu54@gmail.com>
2018-06-23 12:48:00 +0300

    [#158575425] tests(api endpoints)
    -Add more tests for api endpoints

--- tests/test_api.py
+++ tests/test_api.py
@@ -10,6 +10,10 @@ class TestApi(unittest.TestCase):
 
     def setUp(self):
         """Initialize objects for the tests."""
+        self.user = {"Email": "p@g.com",
+                     "Type": "driver"
+                     "Password": "pass123",
+                     "Confirm Password": "pass123"}
         self.ride = {"Destination": "Meru",
                      "Origin": "Kutus",
                      "Time": "9:00",
@@ -20,18 +24,82 @@ class TestApi(unittest.TestCase):
         }
         self.app = create_app('testing')
         self.client = self.app.test_client
+        self.app.context = self.app.context()
         self.app.context.push()
 
     def tearDown(self):
         """Remove objects after test."""
         self.app.context.pop()
         del self.ride
+        del self.user
+        del self.request
+
+    def test_register_user(self):
+        """Test user can register successfuly with correct credentials."""
+        response = self.client().post('/api/v1/auth/register', data=self.user)
+        self.assertEqual(response, 201)
+
+    def test_login(self):
+        """Test user can login with correct credentials."""
+        response = self.client().post('/api/v1/auth/register', data=self.user)
+        self.assertEqual(response, 201)
+        logins = {"Email": "p@g.com", "Password": "pass123"}
+        res = self.client().post('/api/v1/auth/login', data=logins)
+        self.assertTrue(res, 200)
+
+    def test_unregistered_login(self):
+        """Test an unregistered user cannot log in."""
+        logins = {"email": "p@g.com", "Password": "pass123"}
+        res = self.client().post('/api/v1/auth/login', data=logins)
+        self.assertTrue(res, 403)
+
+    def test_log_out(self):
+        """Test user can logout successfuly."""
+        response = self.client().post('api/v1/auth/register', data=self.user)
+        self.assertEqual(response, 201)
+        logins = {"Email": "p@g.com", "Password": "pass123"}
+        res = self.client().post('api/v1/auth/login', data=logins)
+        result = self.client().post('api/v1/auth/logout')
+        self.assertEqual(result, 200)
+
+    def test_edit_profile(self):
+        """Test user can edit their profile."""
+        response = self.client().post('api/v1/auth/register', data=self.user)
+        self.assertEqual(response, 201)
+        logins = {"Email": "p@g.com", "Password": "pass123"}
+        res = self.client().post('api/v1/auth/login', data=logins)
+        details = {"Password": "pass123", "National Id": "34599323",
+                   "Vehicle Registration": "KCD E343"}
+        result = self.client().post('api/v1/auth/profile')
+        self.assertEqual(result, 200)
+
+    def test_get_user_profile(self):
+        """Test user can view their profile."""
+        response = self.client().post('api/v1/auth/register', data=self.user)
+        self.assertEqual(response, 201)
+        logins = {"Email": "p@g.com", "Password": "pass123"}
+        res = self.client().post('api/v1/auth/login', data=logins)
+        details = {"Password": "pass123", "National Id": "34599323",
+                   "Tel No": "+254712705422"}
+        result = self.client().post('api/v1/auth/profile')
+        self.assertEqual(result, 200)
+        result = self.client().get('api/v1/auth/profile')
+        self.assertIn('KCD E343', str(result.data))
 
     def test_create_ride(self):
         """Test user can create a ride successfuly."""
         response = self.client().post('/api/v1/rides', data=self.ride)
         self.assertEqual(response, 201)
         self.assertIn('Meru', str(response.data))
+
+    def test_edit_ride(self):
+        """Test user can edit a ride."""
+        response = self.client().post('/api/v1/rides', data=self.ride)
+        self.assertEqual(response, 201)
+        edit_data = {"Vehicle Color": "Red", "Capacity": 7}
+        res = self.client().put('/api/v1/rides', data=edit_data)
+        self.assertEqual(res.status_code, 200)
+        self.assertIn('Red', str(res.data))
 
     def test_get_ride(self):
         """Test user can get a ride successfuly."""
@@ -48,6 +116,15 @@ class TestApi(unittest.TestCase):
         res = self.client().get('api/v1/rides')
         self.assertEqual(res, 200)
         self.assertIn('Kutus', str(res.data))
+
+    def test_delete_ride(self):
+        """Test user can delete ride."""
+        response = self.client().post('/api/v1/rides', data=self.ride)
+        self.assertEqual(response, 201)
+        res = self.client().delete('/api/v1/rides/1')
+        self.assertEqual(res, 200)
+        result = self.client().get('api/v1/rides/1')
+        self.assertEqual(result, 404)
 
     def test_make_ride_request(self):
         """Test user can make a request successfuly."""
