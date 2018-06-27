4093e23f62d92bab6bee853a869decc2801112fc
Peter <pndungu54@gmail.com>
2018-06-27 23:20:41 +0300

    deploy(heroku)
    -edit procfile to fix heroku deploy issues

--- run.py
+++ run.py
@@ -5,6 +5,4 @@ from app import create_app
 app = create_app('development')
 
 if __name__ == '__main__':
-    app.debug = True
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run()
